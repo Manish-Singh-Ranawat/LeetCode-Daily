39a0e7f159137667fe10b5c5cff652fb65febad2
Manish Singh Ranawat <manish27singhranawat@gmail.com>
2025-02-14 15:24:08 +0530

    Product of the Last K Numbers

---
+++ 2 - February/Feb14__ProductOfLastKNumbers.java
@@ -0,0 +1,80 @@
+// Product of the Last K Numbers - https://leetcode.com/problems/product-of-the-last-k-numbers/description/?envType=daily-question&envId=2025-02-14
+
+// Design an algorithm that accepts a stream of integers and retrieves the product of the last k integers of the stream.
+
+// Implement the ProductOfNumbers class:
+
+// ProductOfNumbers() Initializes the object with an empty stream.
+// void add(int num) Appends the integer num to the stream.
+// int getProduct(int k) Returns the product of the last k numbers in the current list. You can assume that always the current list has at least k numbers.
+// The test cases are generated so that, at any time, the product of any contiguous sequence of numbers will fit into a single 32-bit integer without overflowing.
+
+// Input : ["ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"]
+// [[],[3],[0],[2],[5],[4],[2],[3],[4],[8],[2]]
+
+// Output : [null,null,null,null,null,null,20,40,0,null,32]
+
+// Explanation
+// ProductOfNumbers productOfNumbers = new ProductOfNumbers();
+// productOfNumbers.add(3);        // [3]
+// productOfNumbers.add(0);        // [3,0]
+// productOfNumbers.add(2);        // [3,0,2]
+// productOfNumbers.add(5);        // [3,0,2,5]
+// productOfNumbers.add(4);        // [3,0,2,5,4]
+// productOfNumbers.getProduct(2); // return 20. The product of the last 2 numbers is 5 * 4 = 20
+// productOfNumbers.getProduct(3); // return 40. The product of the last 3 numbers is 2 * 5 * 4 = 40
+// productOfNumbers.getProduct(4); // return 0. The product of the last 4 numbers is 0 * 2 * 5 * 4 = 0
+// productOfNumbers.add(8);        // [3,0,2,5,4,8]
+// productOfNumbers.getProduct(2); // return 32. The product of the last 2 numbers is 4 * 8 = 32 
+
+
+import java.util.ArrayList;
+
+class ProductOfNumbers {
+    ArrayList<Integer> list;
+    int prod;
+
+    public ProductOfNumbers() {
+        this.list = new ArrayList<>();
+        this.prod = 1;
+    }
+
+    public void add(int num) {
+        if (num == 0) {
+            list = new ArrayList<>();
+            prod = 1;
+            return;
+        }
+        prod = prod * num;
+        list.add(prod);
+    }
+
+    public int getProduct(int k) {
+        if (list.size() < k)
+            return 0;
+        int total = list.get(list.size() - 1);
+        if (list.size() == k)
+            return total;
+        return total / list.get(list.size() - 1 - k);
+    }
+}
+
+public class Feb14__ProductOfLastKNumbers {
+    public static void main(String[] args) {
+        ProductOfNumbers productOfNumbers = new ProductOfNumbers();
+
+        productOfNumbers.add(3); // [3]
+        productOfNumbers.add(0); // [3,0]
+        productOfNumbers.add(2); // [3,0,2]
+        productOfNumbers.add(5); // [3,0,2,5]
+        productOfNumbers.add(4); // [3,0,2,5,4]
+
+        System.out.println(productOfNumbers.getProduct(2)); // return 20
+        System.out.println(productOfNumbers.getProduct(3)); // return 40
+        System.out.println(productOfNumbers.getProduct(4)); // return 0
+
+        productOfNumbers.add(8); // [3,0,2,5,4,8]
+
+        System.out.println(productOfNumbers.getProduct(2)); // return 32
+    }
+}
