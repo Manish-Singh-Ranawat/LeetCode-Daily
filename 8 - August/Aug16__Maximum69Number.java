75c27cc4cf25ebd845fe9665cd71b005ff92b4a9
Manish Singh Ranawat <manish27singhranawat@gmail.com>
2025-08-16 18:22:19 +0530

    Maximum 69 Number

---
+++ 8 - August/Aug16__Maximum69Number.java
@@ -0,0 +1,26 @@
+// Maximum 69 Number - https://leetcode.com/problems/maximum-69-number/description/?envType=daily-question&envId=2025-08-16
+
+// You are given a positive integer num consisting only of digits 6 and 9.
+
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+// Input: num = 9669
+// Output: 9969
+// Explanation: 
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666.
+// The maximum number is 9969.
+
+public class Aug16__Maximum69Number {
+    public static int maximum69Number(int num) {
+        return Integer.parseInt(String.valueOf(num).replaceFirst("6", "9"));
+    }
+
+    public static void main(String[] args) {
+        int num = 9669;
+        System.out.println(maximum69Number(num));
+        // 9969
+    }
+}
